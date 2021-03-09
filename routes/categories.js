486e92f0ebfd5eec27dc56f3532640827def727d
Mario <mayitoalmeidamc@gmail.com>
2021-03-08 21:24:32 -0600

    CategoriesServices ok

---
+++ routes/categories.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const categoriesService = require('../controllers/categoriesService')
+
+router.post('/addCategories', categoriesService.addCategories);
+router.delete('/deleteCategories/:idCategoria', categoriesService.deleteCategories);
+router.get('/getCategories', categoriesService.getCategories)
+
+module.exports = router;
